648b93d146c277827ff0f65f48090ffa86032a75
Michael Hansen <michael.hansen.24@us.af.mil>
2020-02-15 08:28:42 -0500

    Starting on new web ui

--- run-with-docker.sh
+++ run-with-docker.sh
@@ -15,8 +15,10 @@ rhasspy_server_version='2.5.0'
 
 # -----------------------------------------------------------------------------
 
+# Name of profile (e.g., "en")
 profile=''
 
+# Parse command-line arguments
 while [[ ! -z "$1" ]];
 do
     if [ "$1" == "--profile" ] || [ "$1" == "-p" ]; then
@@ -35,14 +37,17 @@ fi
 # -----------------------------------------------------------------------------
 
 
+# Determine where to write user profile files.
+# Normally this is in $HOME/.config, but we should check $XDG_CONFIG_HOME first.
 config_home="${XDG_CONFIG_HOME}"
 if [[ -z "${config_home}" ]]; then
     config_home="${HOME}/.config"
 fi
 
-
 user_profiles="${config_home}/rhasspy/profiles"
 
+# Runs Docker container with rhasspy-supervisor tool.
+# This reads a Rhasspy JSON profile and writes a Docker compose YAML file.
 function rhasspy_supervisor {
     docker run -it \
            --user "$(id -u):$(id -g)" \
@@ -51,6 +56,8 @@ function rhasspy_supervisor {
            "$@"
 }
 
+# Runs a Docker container with rhasspy-server-hermes service.
+# The web server frontend to Rhasspy, available on HTTP port 12101.
 function rhasspy_server {
     docker run -it \
            --user "$(id -u):$(id -g)" \
@@ -62,8 +69,13 @@ function rhasspy_server {
 
 # -----------------------------------------------------------------------------
 
+# Clean up profile
 pid_file="${user_profiles}/${profile}/supervisord.pid"
 rm -f "${pid_file}"
+
+export running_file="${user_profiles}/${profile}/.docker_runnning"
+export restart_file="${user_profiles}/${profile}/.restart_docker"
+rm -f "${running_file}" "${restart_file}"
 
 # Generate docker-compose.yml
 rhasspy_supervisor \
@@ -72,11 +84,42 @@ rhasspy_supervisor \
     --debug
 
 compose_file="${user_profiles}/${profile}/docker-compose.yml"
-compose_running=''
+
+# Start Rhasspy services
+docker-compose --file "${compose_file}" up --detach
+touch "${running_file}"
+
+# Watch for a restart signal in the background
+{
+    while true;
+    do
+        sleep 0.5
+        if [[ -f "${restart_file}" ]];
+        then
+            # Restart Rhasspy services
+            echo "Restarting"
+            docker-compose --file "${compose_file}" down --timeout 5
+            rm -f "${running_file}"
+
+            # Start back up
+            docker-compose --file "${compose_file}" up --detach
+            touch "${running_file}"
+
+            # Signal web server that restart is complete
+            rm -f "${restart_file}"
+        fi
+    done
+} &
+
+export restart_pid="$!"
 
 function finish {
     echo "Exiting"
-    if [[ ! -z "${compose_running}" ]]; then
+    if [[ ! -z "${restart_pid}" ]]; then
+        kill "${restart_pid}"
+    fi
+
+    if [[ -f "${running_file}" ]]; then
         # Shut down Docker services if still running
         docker-compose --file "${compose_file}" down
     fi
@@ -84,21 +127,9 @@ function finish {
 
 trap finish EXIT
 
-exit_code="2"
-while [[ "${exit_code}" -eq "2" ]];
-do
-    docker-compose --file "${compose_file}" up --detach
-    compose_running='1'
+# Run web server
+rhasspy_server  \
+    --mqtt-host 'localhost' \
+    --user-profiles "${user_profiles}" \
+    --profile "${profile}"
 
-    # Run web server
-    rhasspy_server  \
-        --mqtt-host 'localhost' \
-        --user-profiles "${user_profiles}" \
-        --profile "${profile}"
-
-    # An exit code of 2 indicates a restart
-    exit_code="$?"
-
-    docker-compose --file "${compose_file}" down --timeout 5
-    compose_running=''
-done
