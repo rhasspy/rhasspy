f44a17276e20ee0d2562cc258f2acc6eea13e685
Michael Hansen <michael.hansen.24@us.af.mil>
2020-03-10 17:34:05 -0400

    Working on batch testing

--- rhasspy-test/scripts/run-tests-for.sh
+++ rhasspy-test/scripts/run-tests-for.sh
@@ -19,18 +19,19 @@ fi
 # Create a temporary directory for profiles
 temp_dir="$(mktemp -d)"
 
-# function cleanup {
-#     rm -rf "${temp_dir}"
-# }
+function cleanup {
+    rm -rf "${temp_dir}"
+}
 
-# trap cleanup EXIT
+trap cleanup EXIT
 
 # -----------------------------------------------------------------------------
 
 function wait-for-url() {
     url="$1"
     echo "Waiting for ${url}"
-    timeout 30 bash -c \
+    # timeout 30 bash -c \
+    bash -c \
             'while [[ "$(curl -s -o /dev/null -w "%{http_code}" "${0}")" != "200" ]]; do sleep 0.5; done' \
             "${url}"
 }
@@ -41,32 +42,58 @@ profiles_dir="${base_dir}/profiles/${lang}"
 shared_dir="${profiles_dir}/shared"
 
 for profile_dir in ${profiles_dir}/test_*; do
+    web_port="$(${base_dir}/scripts/get-free-port)"
+    mqtt_port="$(${base_dir}/scripts/get-free-port)"
     profile_name="$(basename "${profile_dir}")"
-    echo "${lang}/${profile_name}"
+    output_dir="${base_dir}/output/${lang}/${profile_name}"
+    rm -rf "${output_dir}"
+    mkdir -p "${output_dir}"
+
+    echo "Running ${lang}/${profile_name} (http=${web_port}, mqtt=${mqtt_port})"
     cp -R "${profile_dir}" "${temp_dir}/${lang}"
     cp -R "${shared_dir}"/* "${temp_dir}/${lang}/"
 
     user="$(id -u):$(id -g)"
-    container_id="$(docker run -d -v "${temp_dir}:/profiles" --user "${user}" --network host cecep-prime:15555/services/rhasspy:2.5.0 --profile "${lang}" --user-profiles /profiles -- --set download.url_base http://localhost:5000)"
+    docker_command="docker run -d -v "${temp_dir}:/profiles" --user "${user}" --network host cecep-prime:15555/services/rhasspy:2.5.0 --profile "${lang}" --user-profiles /profiles --http-port ${web_port} --local-mqtt-port ${mqtt_port} -- --set download.url_base 'http://localhost:5000'"
+    echo "${docker_command}"
+
+    container_id="$(${docker_command})"
 
     (
-        wait-for-url 'http://localhost:12101/api/version' || exit 1
+        # Block until Rhasspy web server is ready
+        wait-for-url "http://localhost:${web_port}/api/version" || exit 1
         echo ''
 
-        echo "Downloading"
-        curl -X POST 'http://localhost:12101/api/download-profile' || exit 1
+        # Download all profle artifacts
+        echo "Downloading..."
+        curl -X POST "http://localhost:${web_port}/api/download-profile" || exit 1
         echo ''
 
-        echo "Restarting"
-        curl -X POST 'http://localhost:12101/api/restart' || exit 1
+        # Re-start services
+        echo "Restarting..."
+        curl -X POST "http://localhost:${web_port}/api/restart" || exit 1
         echo ''
 
-        echo "Training"
-        curl -X POST 'http://localhost:12101/api/train' || exit 1
+        # Train profile
+        echo "Training..."
+        curl -X POST "http://localhost:${web_port}/api/train" || exit 1
         echo ''
 
-        echo "Testing"
-        (cd "${base_dir}" && python3 -m unittest tests/${lang}/*.py) || exit 1
-        echo ''
+        # Run tests
+        echo "Testing..."
+        wav_archive="${temp_dir}/${profile_name}.tar.gz"
+        tar -czf "${wav_archive}" "${base_dir}/wav/${lang}" 2>/dev/null
+        curl -s -X POST -F "archive=@${wav_archive}" "http://localhost:${web_port}/api/evaluate" | \
+            tee "${output_dir}/response.txt" | \
+            jq . > "${output_dir}/report.json"
+
+        # (cd "${base_dir}" && python3 -m unittest tests/${lang}/*.py) > "${output_dir}/test.txt" || exit 1
+        echo 'OK'
     ) || docker stop "${container_id}"
+
+    echo 'Stopping Docker container...'
+    docker stop "${container_id}"
+    echo "Finished ${profile_name}"
+    echo '----------'
+    echo ''
 done
