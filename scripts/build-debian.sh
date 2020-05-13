bd8d2c07cb8ea181e5ea58b77de6ff6309e14b06
Michael Hansen <michael.hansen.24@us.af.mil>
2020-05-13 09:43:46 -0400

    Started work on Debian build

--- scripts/build-debian.sh
+++ scripts/build-debian.sh
@@ -1,94 +1,89 @@
 #!/usr/bin/env bash
 set -e
 
-architecture="$1"
-version="$2"
-
-if [[ -z "${version}" ]];
-then
-    echo "Usage: build-debian.sh architecture version"
+if [[ -z "$1" ]]; then
+    echo "Usage: build-debian dist/ [target] [target]"
     exit 1
 fi
+
+dist_dir="$(realpath "$1")"
+mkdir -p "${dist_dir}"
+shift
 
 # Directory of *this* script
 this_dir="$( cd "$( dirname "$0" )" && pwd )"
 src_dir="$(realpath "${this_dir}/..")"
 
-package_name='rhasspy'
-python_name='rhasspy'
+version="$(cat "${src_dir}/VERSION")"
 
-venv="${src_dir}/.venv"
-if [[ -d "${venv}" ]]; then
-    echo "Using virtual environment at ${venv}"
-    source "${venv}/bin/activate"
+# -----------------------------------------------------------------------------
+
+if [[ -z "${NOBUILDX}" ]]; then
+    if [[ -z "$1" ]]; then
+        platforms=('linux/amd64' 'linux/arm/v7' 'linux/arm64')
+    else
+        platforms=("$@")
+    fi
+
+    function join { local IFS="$1"; shift; echo "$*"; }
+    platform_str=$(join ',' ${platforms[@]})
+else
+    if [[ -z "$1" ]]; then
+        echo "Platform is required with NOBUILDX"
+        exit 1
+    fi
+
+    platforms=("$@")
 fi
 
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-dist="${src_dir}/dist"
-mkdir -p dist
+: "${DOCKER_REGISTRY=docker.io}"
 
-# Create PyInstaller artifacts
-dist_dir="${src_dir}/pyinstaller/dist"
-pyinstaller \
-    -y \
-    --workpath "${src_dir}/pyinstaller/build" \
-    --distpath "${dist_dir}" \
-    "${python_name}.spec"
+echo "Building..."
 
-# Delete unnecessary directories
-rm -rf \
-   "${pyinstaller_dist}/${python_name}/share/" \
-   "${pyinstaller_dist}${python_name}/notebook/"
+if [[ -z "${NOBUILDX}" ]]; then
+    docker buildx build \
+           "${src_dir}" \
+           -f "${src_dir}/Dockerfile.debian" \
+           "--platform=${platform_str}" \
+           --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
+           --tag "${DOCKER_REGISTRY}/rhasspy-debian" \
+           --push
+else
+    docker build \
+           "${src_dir}"  \
+           -f "${src_dir}/Dockerfile.debian" \
+           --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
+           -t "${DOCKER_REGISTRY}/rhasspy-debian"
+fi
 
-download_dir="${src_dir}/download"
+# ------------------------------------------------------------------------------
 
-# Copy required files
-mkdir -p "${dist_dir}/${python_name}/rhasspyprofile"
-cp -R "${src_dir}/rhasspy-profile/rhasspyprofile/profiles" \
-   "${dist_dir}/${python_name}/rhasspyprofile/"
+declare -A platform_to_debian
+platform_to_debian=(['linux/amd64']='amd64' ['linux/arm/v6']='armel' ['linux/arm/v7']='armhf' ['linux/arm64']='arm64')
 
-mkdir -p "${dist_dir}/${python_name}/rhasspyserver_hermes"
-cp -R "${src_dir}/rhasspy-server-hermes/web" \
-   "${src_dir}/rhasspy-server-hermes/templates" \
-   "${src_dir}/VERSION" \
-   "${dist_dir}/${python_name}/rhasspyserver_hermes"
+for platform in "${platforms[@]}"; do
+    echo "Packaging..."
+    debian_arch="${platform_to_debian["${platform}"]}"
+    package_name="rhasspy_${version}_${debian_arch}"
 
-# Copy pre-built programs
-mkdir -p "${dist_dir}/rhasspy/phonetisaurus"
-tar -C "${dist_dir}/rhasspy/phonetisaurus" \
-    -xvf "${download_dir}/phonetisaurus-2019-${architecture}.tar.gz"
+    if [[ -z "${NOBUILDX}" ]]; then
+        docker pull \
+               --platform "${platform}" \
+               "${DOCKER_REGISTRY}/rhasspy-debian"
 
-mkdir -p "${dist_dir}/rhasspy/rhasspyasr_kaldi"
-tar -C "${dist_dir}/rhasspy/rhasspyasr_kaldi" \
-    -xvf "${download_dir}/kaldi-2020-${architecture}.tar.gz"
+        docker run \
+               --platform "${platform}" \
+               --rm -i --entrypoint /bin/cat \
+               "${DOCKER_REGISTRY}/rhasspy-debian" \
+               "/build/${package_name}.deb" > "${dist_dir}/${package_name}.deb"
+    else
+        docker run \
+               --rm -i --entrypoint /bin/cat \
+               "${DOCKER_REGISTRY}/rhasspy-debian" \
+               "/build/${package_name}.deb" > "${dist_dir}/${package_name}.deb"
+    fi
 
-# Tar up binary distribution
-tar -C "${src_dir}/pyinstaller/dist" \
-    -czf \
-    "${dist}/${package_name}_${version}_${architecture}.tar.gz" \
-    "${python_name}/"
-
-# Create Debian package
-debian_package="${package_name}_${version}_${architecture}"
-debian="${src_dir}/debian"
-debian_dir="${debian}/${debian_package}"
-
-rm -rf "${debian_dir}"
-mkdir -p "${debian_dir}/DEBIAN" "${debian_dir}/usr/bin" "${debian_dir}/usr/lib"
-
-# Generate control file
-version="${version}" architecture="${architecture}" envsubst \
-    <"${debian}/DEBIAN/control" >"${debian_dir}/DEBIAN/control"
-
-# Copy bin/lib files
-cp "${debian}/bin"/* "${debian_dir}/usr/bin/"
-cp -R "${src_dir}/pyinstaller/dist/${python_name}" "${debian_dir}/usr/lib/"
-
-# Build .deb package
-cd "${debian}/" && fakeroot dpkg --build "${debian_package}"
-mv "${debian}/${debian_package}.deb" "${dist}/"
-
-# -----------------------------------------------------------------------------
-
-echo "OK"
+    echo "Wrote ${dist_dir}/${package_name}.deb"
+done
