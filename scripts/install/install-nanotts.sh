19915437c5c51aedc32cd98ad66335e023661a53
Michael Hansen <michael.hansen.24@us.af.mil>
2020-05-29 08:59:29 -0400

    Use correct lang dir in nanotts install

--- scripts/install/install-nanotts.sh
+++ scripts/install/install-nanotts.sh
@@ -11,5 +11,16 @@ output="$(realpath "$2")"
 
 mkdir -p "${output}/lib/nanotts"
 tar -C "${output}/lib/nanotts" --strip-components=1 -xf "${nanotts}"
-mv "${output}/lib/nanotts/pico/lang" "${output}/lib/nanotts/"
-ln -sf "${output}/lib/nanotts/nanotts" "${output}/bin/nanotts"
+
+# Write wrapper script that uses correct lang dir
+(
+cat <<'EOF'
+#!/usr/bin/env bash
+this_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+nanotts_dir="$(realpath "${this_dir}/../lib/nanotts")"
+"${nanotts_dir}/nanotts" -l "${nanotts_dir}/pico/lang" "$@"
+EOF
+) > "${output}/bin/nanotts"
+
+chmod +x "${output}/bin/nanotts"
